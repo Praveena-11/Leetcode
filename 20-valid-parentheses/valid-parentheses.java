bcacadca2600b7aa41c6924df97e01484442809b
Praveena Kalidasan <praveenakalidasan11@gmail.com>
2026-01-15 21:40:12 +0530

    Time: 3 ms (87.62%) | Memory: 43.2 MB (61.84%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,18 +1,23 @@
-class Solution {
-  public boolean isValid(String s) {
-    Deque <Character> stack = new ArrayDeque<>();
-    for(final char c:s.toCharArray()){
-        if(c=='('){
-            stack.push(')');
-        }else if(c=='['){
-            stack.push(']');
-        }else if(c=='{'){
-            stack.push('}');
-        }else if(stack.isEmpty() || stack.pop()!=c){
-            return false;
-        }
-    }
-    return stack.isEmpty();
+import java.util.Stack;
 
-  }
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == ']' && top != '[') ||
+                    (c == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
 }
