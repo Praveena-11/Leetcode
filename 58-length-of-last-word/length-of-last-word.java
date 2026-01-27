171a13509087a0a59ce7dbad5082a62e7fc6eb54
Praveena Kalidasan <praveenakalidasan11@gmail.com>
2026-01-27 14:44:29 +0530

    Time: 1 ms (34.88%) | Memory: 43.4 MB (12.43%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,8 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] arr=s.split("\\s+");
+        String[] arr=s.split(" ");
         String last=arr[arr.length-1];
         return last.length();
-
     }
 }
